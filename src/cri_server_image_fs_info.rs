6fc721157c47292c30552fde5ec25316193033d2
lurui <lurui@ipfsforce.com>
2021-04-14 14:38:56 +0800

    微调

--- src/cri_server_image_fs_info.rs
+++ src/cri_server_image_fs_info.rs
@@ -7,7 +7,9 @@ pub async fn cri_image_fs() -> (i64,String,u64,u64) {
     let used_bytes:u64;
     let inodes_used:u64;
 
-    (used_bytes,inodes_used) = computer_fs_info();
+    let computer_result = computer_fs_info();
+    used_bytes = computer_result.0;
+    inodes_used = computer_result.1;
 
     return (timestamp,mountpoint,used_bytes,inodes_used)
 }
